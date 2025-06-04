218234d0b073e0db2b53bb65c3b429cb417b88e5
deitolumens <2421037310003@mhs.ulm.ac.id>
2025-06-05 03:16:05 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AquariumDashboard from './components/AquariumDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AquariumDashboard />
+    </div>
+  );
+}
+
+export default App;
