6d061c1a65fe10fb0848d4a1e0b00a5e9b1a737e
deitolumens <2421037310003@mhs.ulm.ac.id>
2025-06-10 02:42:44 +0800

    Update polyfills.js

--- src/polyfills.js
+++ src/polyfills.js
@@ -5,6 +5,16 @@ import * as url from 'url';
 window.Buffer = Buffer;
 window.url = url;
 
+// Add WebSocket polyfill for React
+if (typeof WebSocket === 'undefined') {
+  global.WebSocket = require('ws');
+}
+
+// Buffer polyfill
+if (typeof Buffer === 'undefined') {
+  global.Buffer = require('buffer').Buffer;
+}
+
 if (!window.process) {
   window.process = {
     env: {},
